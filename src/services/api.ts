8c7e191b397bb2d47f3dbf0324c4fba3e1b2ea4d
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2021-05-24 17:57:11 -0300

    update api route

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://api.valorize.pivovar.com.br/api/",
+  baseUrl: "http://figueirao.valorize.pivovar.com.br/api/",
+  // baseURL: "http://api.valorize.pivovar.com.br/api/",
   // baseURL: "https://valorize-sua-cidade.herokuapp.com",
   // baseURL: "http://localhost:3333/",
 });
